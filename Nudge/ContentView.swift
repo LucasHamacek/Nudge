c90882f2fb4c08ca29bb54f87afd8bae5e2578cc
Lucas <lucashamacek@hotmail.com>
2025-12-09 15:48:47 -0300

    Initial Commit

---
+++ Nudge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nudge
+//
+//  Created by Joao Hamacek on 09/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Joao Hamacek on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
